43af345fdf5e936658cb292699b8dabd2c3a4c9a
Lafi Raed <browniesandmuffin@gmail.com>
2024-11-16 18:27:11 +0100

    xd

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Hero from "@/components/Hero/Hero";
 import "./page.css";
 import HowItWorks from "@/components/HowItWorks/HowItWorks";
+import LookingFor from "@/components/LookingFor/LookingFor";
 export default function Home() {
   return (
     <div className="mx-auto flex w-full flex-col">
       <Hero/>
       <HowItWorks/>
+      <LookingFor/>
     </div>
   );
 }
